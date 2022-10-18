5fda0d1ff9c89036c9030be7174868f172e1aed3
ang-xd <angxdxdd@gmail.com>
2022-10-18 19:56:56 -0400

    Provide more information on the example's registration

--- common/src/main/java/com/kyanite/paragon/example/TestMod.java
+++ common/src/main/java/com/kyanite/paragon/example/TestMod.java
@@ -6,8 +6,10 @@ import com.kyanite.paragon.api.enums.ConfigType;
 
 public class TestMod {
     public void init() {
+        // Expandable registration system
         ConfigRegistry.register("testmod", new TestModConfig());
 
+        // Lightweight registration system
         ConfigHolder configHolder = new ConfigHolder("testmod", ConfigType.STANDARD,
             TestModConfig.isSus, TestModConfig.name, TestModConfig.sussiness);
     }
