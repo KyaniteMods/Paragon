26216884a5ee5e02f4c479326079a624565753d3
ang-xd <angxdxdd@gmail.com>
2022-12-11 10:05:50 -0500

    Release workflow?

--- common/src/main/java/com/kyanite/paragon/api/interfaces/Description.java
+++ common/src/main/java/com/kyanite/paragon/api/interfaces/Description.java
@@ -1,5 +1,9 @@
 package com.kyanite.paragon.api.interfaces;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Description {
     String value();
 }
